9beb5c87f5fa38cbb7508776fbac5e1f1fa627fa
Ahmed S. <s.ahmed.profiquence@gmail.com>
2025-11-18 10:10:19 +0530

    Solved LeetCode 0717 - 1-bit and 2-bit Characters

---
+++ LeetCode/0717_1-bit_and_2-bit_Characters/main.cpp
@@ -0,0 +1,20 @@
+#include <vector>
+
+using namespace std;
+
+class Solution
+{
+public:
+    bool isOneBitCharacter(vector<int> &bits)
+    {
+        int n = bits.size();
+        int i = 0;
+
+        while (i < n - 1)
+        {
+            i += bits[i] + 1;
+        }
+
+        return i == n - 1;
+    }
+};
