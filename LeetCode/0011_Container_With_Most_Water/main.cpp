27ee1ff34711f22bd24007fec26a2563e6e908fe
Ahmed S. <s.ahmed.profiquence@gmail.com>
2025-11-27 11:46:49 +0530

    Solved LeetCode 0011 - Container with most water

---
+++ LeetCode/0011_Container_With_Most_Water/main.cpp
@@ -0,0 +1,32 @@
+#include <vector>
+
+using namespace std;
+
+class Solution
+{
+public:
+    int maxArea(vector<int> &height)
+    {
+        int maxArea = 0;
+
+        int left = 0;
+        int right = height.size() - 1;
+
+        while (left < right)
+        {
+            int currentArea = (right - left) * min(height[left], height[right]);
+            maxArea = max(maxArea, currentArea);
+
+            if (height[left] <= height[right])
+            {
+                left++;
+            }
+            else
+            {
+                right--;
+            }
+        }
+
+        return maxArea;
+    }
+};
