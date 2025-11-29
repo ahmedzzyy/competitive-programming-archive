2bc1e756515a427a89a7eaaae485bbc36b7021c1
Ahmed S. <s.ahmed.profiquence@gmail.com>
2025-11-29 13:15:25 +0530

    Solved LeetCode 0704 - Binary Search

---
+++ LeetCode/0704_Binary_Search/main.cpp
@@ -0,0 +1,35 @@
+#include <vector>
+
+using namespace std;
+
+class Solution
+{
+public:
+    int search(vector<int> &nums, int target)
+    {
+        int left = 0, right = nums.size() - 1;
+
+        while (left <= right)
+        {
+            // int mid = (left + right) / 2;
+            // To avoid possible overflow
+            int mid = left + ((right - left) / 2);
+            // left + half of the distance
+
+            if (nums[mid] == target)
+            {
+                return mid;
+            }
+            else if (nums[mid] > target)
+            {
+                right = mid - 1;
+            }
+            else
+            {
+                left = mid + 1;
+            }
+        }
+
+        return -1;
+    }
+};
