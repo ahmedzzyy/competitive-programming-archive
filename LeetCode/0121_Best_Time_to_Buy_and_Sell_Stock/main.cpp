3ea1c9968a528029cfd542a7b91885c6f26ae543
Ahmed S. <s.ahmed.profiquence@gmail.com>
2025-12-06 12:07:45 +0530

    Solved LeetCode 0121 - Best Time to Buy and Sell Stock

---
+++ LeetCode/0121_Best_Time_to_Buy_and_Sell_Stock/main.cpp
@@ -0,0 +1,29 @@
+#include <vector>
+
+using namespace std;
+
+class Solution
+{
+public:
+    int maxProfit(vector<int> &prices)
+    {
+        int left = 0, right = 1;
+        int maxProfit = 0;
+
+        while (right < prices.size())
+        {
+            if (prices[left] < prices[right])
+            {
+                maxProfit = max(maxProfit, prices[right] - prices[left]);
+            }
+            else
+            {
+                left = right;
+            }
+
+            right++;
+        }
+
+        return maxProfit;
+    }
+};
